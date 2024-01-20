db760113dee3e23111c5817fbf1f444f3e2fbeb0
Muaviya Imran <muaviyaimran1122@gmail.com>
2024-01-20 22:11:33 +0500

    Implemented HR Screens remaiaing screens are send salary and preview pdf

--- main.go
+++ main.go
@@ -14,7 +14,16 @@ func main() {
 
 	r.Handle("/css/*", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
 	r.Handle("/assets/*", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	
     r.Get("/",handlers.H1)
+	r.Get("/employees",handlers.H2)
+	r.Get("/employee-details",handlers.H3)
+	r.Get("/employee-details",handlers.H4)
+	r.Get("/employee-technical",handlers.H5)
+	r.Get("/employee-documents",handlers.H6)
+	r.Get("/employee-salary",handlers.H7)
+	r.Get("/transact-salaries",handlers.H8)
+	
 
 	log.Println("Listening on port 3000...")
 	log.Fatal(http.ListenAndServe(":3000", r))
