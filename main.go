3933680d2721d2d91ffda55322664f5a4b750eaa
Muaviya Imran <muaviyaimran1122@gmail.com>
2024-01-29 10:58:24 +0500

    Fixed Direct Routes for Third Level Components

--- main.go
+++ main.go
@@ -12,18 +12,17 @@ import (
 func main() {
 	r := chi.NewRouter()
 
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	r.Handle("/css/*", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
 	r.Handle("/assets/*", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
-	
+
     r.Get("/",handlers.H1)
 	r.Get("/employees",handlers.H2)
-	r.Get("/employee-details",handlers.H3)
+	r.Get("/transact-salaries",handlers.H3)
 	r.Get("/employee-details",handlers.H4)
 	r.Get("/employee-technical",handlers.H5)
 	r.Get("/employee-documents",handlers.H6)
 	r.Get("/employee-salary",handlers.H7)
-	r.Get("/transact-salaries",handlers.H8)
-	
 
 	log.Println("Listening on port 3001...")
 	log.Fatal(http.ListenAndServe(":3001", r))
